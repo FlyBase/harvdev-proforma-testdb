652fd0f361fcb6e89f8403b44da21b0b984d23f5
ianlongden <ianlongden@morgan.harvard.edu>
2019-05-29 15:44:01 -0400

    added pubauthor

--- add-test_data.py
+++ add-test_data.py
@@ -99,7 +99,7 @@ cv_cvterm = {'FlyBase': ['FlyBase analysis'],
                     'natural_transposable_element', 'gene_group'],
              'synonym type': ['fullname', 'symbol', 'unspecified'],
              'pub type': ['computer file', 'unattributed', 'unspecified', 'personal communication to FlyBase', 'perscommtext'],
-             'pubprop type': ['curated_by', 'languages', 'perscommtext', 'cam_flag', 'harv_flag'],
+             'pubprop type': ['curated_by', 'languages', 'perscommtext', 'cam_flag', 'harv_flag', 'associated_text'],
              'relationship type': ['associated_with', 'belongs_to', 'attributed_as_expression_of'],
              'property type': ['comment', 'reported_genomic_loc', 'origin_comment', 'description', 'molobject_type',
                                'in_vitro_progenitor', 'balancer_status', 'members_in_db', 'data_link', 'stage',
@@ -170,20 +170,25 @@ cursor.execute(cvterm_sql, (dbxref_id, cv_id['FlyBase miscellaneous CV'], 'umbre
 cvterm_id['umbrella project'] = cursor.fetchone()[0]
 cursor.execute(cvprop_sql, (cvterm_id['umbrella project'], cvterm_id['webcv'], 'project_type'))
 
+author_id = 1
+author_sql = """ INSERT INTO pubauthor (pubauthor_id, pub_id, rank, surname, givennames) VALUES (%s, %s, %s, %s, %s) """
 # create pubs
 pub_id = 0
-pub_sql = """ INSERT INTO pub (type_id, title, uniquename) VALUES (%s, %s, %s) RETURNING pub_id """
+pub_sql = """ INSERT INTO pub (type_id, title, uniquename, pyear) VALUES (%s, %s, %s, %s) RETURNING pub_id """
 pubprop_sql = """ INSERT INTO pubprop (pub_id, type_id, value, rank) VALUES (%s, %s, %s, %s) """
-cursor.execute( pub_sql, (cvterm_id['computer file'], 'Nature', 'FBrf0000001'))
+cursor.execute( pub_sql, (cvterm_id['computer file'], 'Nature', 'FBrf0000001', '1967'))
 pub_id = cursor.fetchone()[0]
 cursor.execute( pubprop_sql, (pub_id, cvterm_id['curated_by'], "Curator:bob McBob....", pub_id))
+cursor.execute( author_sql,( author_id, pub_id, 1, "Bueller", "Ferris"))
+author_id += 1
+cursor.execute( author_sql,( author_id, pub_id, 2, "Bueller", "Lesser"))
 
-cursor.execute( pub_sql, (cvterm_id['unattributed'], 'unattributed', 'unattributed'))
+cursor.execute( pub_sql, (cvterm_id['unattributed'], 'unattributed', 'unattributed', '1973'))
 pub_id = cursor.fetchone()[0]
 cursor.execute( pubprop_sql, (pub_id, cvterm_id['curated_by'], "Curator:bob McBob....", pub_id))
 
 for fly_ref in ('FBrf0104946', 'FBrf0105495'):
-    cursor.execute( pub_sql, (cvterm_id['FlyBase analysis'], 'predefined pubs {}'.format(fly_ref[:3]), fly_ref))
+    cursor.execute( pub_sql, (cvterm_id['FlyBase analysis'], 'predefined pubs {}'.format(fly_ref[:3]), fly_ref ,'2000'))
     pub_id = cursor.fetchone()[0]
     cursor.execute( pubprop_sql, (pub_id, cvterm_id['curated_by'], "Curator:bob McBob....", pub_id))
 
