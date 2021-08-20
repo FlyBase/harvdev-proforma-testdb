b2e355fa0a05cbb86169bd1781fe47ee226cb5e9
ianlongden <ianlongden@morgan.harvard.edu>
2021-08-20 17:09:03 -0400

    added some grpprop values

--- Load/grp.py
+++ Load/grp.py
@@ -3,17 +3,17 @@ grp_syn_sql = """ INSERT INTO grp_synonym (grp_id, synonym_id, pub_id, is_curren
 syn_sql = """ INSERT INTO synonym (name, type_id, synonym_sgml) VALUES (%s, %s, %s) RETURNING synonym_id """
 grp_dbxref_sql = """ INSERT INTO grp_dbxref (grp_id, dbxref_id) VALUES (%s, %s) """
 grp_cvterm_sql = """ INSERT INTO grp_cvterm (grp_id, cvterm_id, pub_id) VALUES (%s, %s, %s) """
-grp_prop_sql = """ INSERT INTO grpprop (grp_id, type_id) VALUES (%s, %s) RETURNING grpprop_id """
+grp_prop_sql = """ INSERT INTO grpprop (grp_id, type_id, value) VALUES (%s, %s, %s) RETURNING grpprop_id """
 grp_proppub_sql = """ INSERT INTO grpprop_pub (grpprop_id, pub_id) VALUES (%s, %s) """
 grp_pub_sql = """ INSERT INTO grp_pub (grp_id, pub_id) VALUES (%s, %s) """
 
 
 def add_grp_data(cursor, feature_id, cvterm_id, dbxref_id, pub_id):
     """Load gene grp info"""
-    for i in range(1, 12):
+    for i in range(1, 15):
         # grp
         cursor.execute(grp_sql, ('grp-{}'.format(i), 'FBgg:temp_{}'.format(i), cvterm_id['gene_group']))
-        grp_id = cursor.fetchone()[0]
+        feature_id['grp-{}'.format(i)] = grp_id = cursor.fetchone()[0]
 
         # grp fullname syn
         cursor.execute(syn_sql, ('grp-fullname-{}'.format(i), cvterm_id['fullname'], 'grp-fullname-{}'.format(i)))
@@ -22,9 +22,17 @@ def add_grp_data(cursor, feature_id, cvterm_id, dbxref_id, pub_id):
 
         # grp symbol syn
         cursor.execute(syn_sql, ('grp-{}'.format(i), cvterm_id['symbol'], 'grp-{}'.format(i)))
-        feature_id['grp-{}'.format(i)] = syn_id = cursor.fetchone()[0]
+        syn_id = cursor.fetchone()[0]
         cursor.execute(grp_syn_sql, (grp_id, syn_id, feature_id['unattributed'], True))
         cursor.execute(grp_syn_sql, (grp_id, syn_id, pub_id, True))
+
+        # add alternative synonyms
+        cursor.execute(syn_sql, ('alt-grp-{}'.format(i), cvterm_id['symbol'], 'alt-grp-{}'.format(i)))
+        syn_id = cursor.fetchone()[0]
+        cursor.execute(grp_syn_sql, (grp_id, syn_id, pub_id, False))
+        cursor.execute(syn_sql, ('alt2-grp-{}'.format(i), cvterm_id['symbol'], 'alt2-grp-{}'.format(i)))
+        syn_id = cursor.fetchone()[0]
+        cursor.execute(grp_syn_sql, (grp_id, syn_id, pub_id, False))
 
         # add pubs
         cursor.execute(grp_pub_sql, (grp_id, pub_id))
@@ -37,7 +45,7 @@ def add_grp_data(cursor, feature_id, cvterm_id, dbxref_id, pub_id):
         cursor.execute(grp_cvterm_sql, (grp_id, cvterm_id['functional group'], pub_id))
 
         # add prop
-        cursor.execute(grp_prop_sql, (grp_id, cvterm_id['gg_owner']))
+        cursor.execute(grp_prop_sql, (grp_id, cvterm_id['gg_owner'], 'owner-{}'.format(i)))
         grpprop_id = cursor.fetchone()[0]
 
         # add proppub
