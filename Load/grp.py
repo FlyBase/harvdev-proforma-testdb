c12296ba6ee360299c554cc315d5877242c4624c
ianlongden <ianlongden@morgan.harvard.edu>
2021-08-24 11:50:01 -0400

    Add multiple grp props for testing bangd

--- Load/grp.py
+++ Load/grp.py
@@ -3,6 +3,7 @@ grp_syn_sql = """ INSERT INTO grp_synonym (grp_id, synonym_id, pub_id, is_curren
 syn_sql = """ INSERT INTO synonym (name, type_id, synonym_sgml) VALUES (%s, %s, %s) RETURNING synonym_id """
 grp_dbxref_sql = """ INSERT INTO grp_dbxref (grp_id, dbxref_id) VALUES (%s, %s) """
 grp_cvterm_sql = """ INSERT INTO grp_cvterm (grp_id, cvterm_id, pub_id) VALUES (%s, %s, %s) """
+grp_prop_rank_sql = """ INSERT INTO grpprop (grp_id, type_id, value, rank) VALUES (%s, %s, %s, %s) RETURNING grpprop_id """
 grp_prop_sql = """ INSERT INTO grpprop (grp_id, type_id, value) VALUES (%s, %s, %s) RETURNING grpprop_id """
 grp_proppub_sql = """ INSERT INTO grpprop_pub (grpprop_id, pub_id) VALUES (%s, %s) """
 grp_pub_sql = """ INSERT INTO grp_pub (grp_id, pub_id) VALUES (%s, %s) """
@@ -51,11 +52,19 @@ def add_grp_data(cursor, feature_id, cvterm_id, dbxref_id, pub_id):
         # add prop
         cursor.execute(grp_prop_sql, (grp_id, cvterm_id['gg_owner'], 'owner-{}'.format(i)))
         grpprop_id = cursor.fetchone()[0]
-
         # add proppub
         cursor.execute(grp_proppub_sql, (grpprop_id, pub_id))
 
-        # add parent relatinship
+        # add more of one type to test bangd
+        cursor.execute(grp_prop_rank_sql, (grp_id, cvterm_id['gg_description'], 'line1', 0))
+        grpprop_id = cursor.fetchone()[0]
+        cursor.execute(grp_proppub_sql, (grpprop_id, pub_id))
+
+        cursor.execute(grp_prop_rank_sql, (grp_id, cvterm_id['gg_description'], 'line2', 1))
+        grpprop_id = cursor.fetchone()[0]
+        cursor.execute(grp_proppub_sql, (grpprop_id, pub_id))
+
+        # add parent relationship
         if i > 1:
             cursor.execute(grp_relationship_sql, (grp_id, feature_id['grp-{}'.format(i-1)], cvterm_id['parent_grp']))
             grp_rel_id = cursor.fetchone()[0]
