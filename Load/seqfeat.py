b8eb9cb52b20142a814bd1c92e8dd4bf0dfdd6f2
ianlongden <ianlongden@morgan.harvard.edu>
2022-11-22 15:08:33 -0500

    More seqfeat data

--- Load/seqfeat.py
+++ Load/seqfeat.py
@@ -14,11 +14,13 @@ def add_seqfeat_data(cursor, cvterm_id, organism_id, dbxref_id, pub_id, db_id, f
                  VALUES (%s, %s) """
 
     featprop_sql = """ INSERT INTO featureprop (feature_id, type_id, rank, value)
-                       VALUES (%s, %s, %s, %s) """
+                       VALUES (%s, %s, %s, %s) RETURNING featureprop_id"""
+    fppub_sql = """ INSERT INTO featureprop_pub (featureprop_id, pub_id) VALUES (%s, %s) """
 
     feat_rel_sql = """ INSERT INTO feature_relationship (subject_id, object_id,  type_id)
                        VALUES (%s, %s, %s) RETURNING feature_relationship_id """
-    frp_sql = """ INSERT INTO feature_relationshipprop (feature_relationship_id, type_id, value, rank) VALUES (%s, %s, %s, %s) RETURNING  feature_relationshipprop_id"""
+    frp_sql = """ INSERT INTO feature_relationshipprop (feature_relationship_id, type_id, value, rank)
+                  VALUES (%s, %s, %s, %s) RETURNING  feature_relationshipprop_id"""
     frpub_sql = """ INSERT INTO feature_relationshipprop_pub (feature_relationshipprop_id, pub_id) VALUES (%s, %s) """
 
     obsolete = False
@@ -39,19 +41,22 @@ def add_seqfeat_data(cursor, cvterm_id, organism_id, dbxref_id, pub_id, db_id, f
         syn_id = cursor.fetchone()[0]
         cursor.execute(fs_sql, (syn_id, sf_id, pub_id, True))
 
-        cursor.execute(syn_sql, ('seqfeat-{}'.format(i+1), cvterm_id['fullname'], 'seqfeat-{}'.format(i+1)))
-        syn_id = cursor.fetchone()[0]
-        cursor.execute(fs_sql, (syn_id, sf_id, pub_id, True))
-        cursor.execute(fs_sql, (syn_id, sf_id, feature_id['unattributed'], True))
+        ####################################
+        # seq feat have NO fullname synonyms
+        ####################################
+        # cursor.execute(syn_sql, ('seqfeat-{}'.format(i+1), cvterm_id['fullname'], 'seqfeat-{}'.format(i+1)))
+        # syn_id = cursor.fetchone()[0]
+        # cursor.execute(fs_sql, (syn_id, sf_id, pub_id, True))
+        # cursor.execute(fs_sql, (syn_id, sf_id, feature_id['unattributed'], True))
 
-        # seqfeature-X [symbol,fullname], is_current FALSE
+        # seqfeature-X [symbol], is_current FALSE
         cursor.execute(syn_sql, ('seqfeature-{}'.format(i+1), cvterm_id['symbol'], 'seqfeature-{}'.format(i+1)))
         syn_id = cursor.fetchone()[0]
         cursor.execute(fs_sql, (syn_id, sf_id, pub_id, False))
 
-        cursor.execute(syn_sql, ('seqfeature-{}'.format(i+1), cvterm_id['fullname'], 'seqfeature-{}'.format(i+1)))
-        syn_id = cursor.fetchone()[0]
-        cursor.execute(fs_sql, (syn_id, sf_id, pub_id, False))
+        # cursor.execute(syn_sql, ('seqfeature-{}'.format(i+1), cvterm_id['fullname'], 'seqfeature-{}'.format(i+1)))
+        # syn_id = cursor.fetchone()[0]
+        # cursor.execute(fs_sql, (syn_id, sf_id, pub_id, False))
 
         # Feat pub
         cursor.execute(feat_pub_sql, (sf_id, feature_id['unattributed']))
@@ -67,14 +72,27 @@ def add_seqfeat_data(cursor, cvterm_id, organism_id, dbxref_id, pub_id, db_id, f
         ############
         # Add props.
         ############
+
         # internal notes
         cursor.execute(featprop_sql, (sf_id, cvterm_id['internalnotes'], 0, f"inote_{i+1}_1"))
+        fp_id = cursor.fetchone()[0]
+        cursor.execute(fppub_sql, (fp_id, pub_id))
+
         # evidence
         cursor.execute(featprop_sql, (sf_id, cvterm_id['evidence'], 0, f"evidence_{i+1}_1"))
+        fp_id = cursor.fetchone()[0]
+        cursor.execute(fppub_sql, (fp_id, pub_id))
         cursor.execute(featprop_sql, (sf_id, cvterm_id['evidence'], 1, f"evidence_{i+1}_2"))
+        fp_id = cursor.fetchone()[0]
+        cursor.execute(fppub_sql, (fp_id, pub_id))
+
         # linked_to
         cursor.execute(featprop_sql, (sf_id, cvterm_id['linked_to'], 0, f"linked_to_{i+1}_1"))
+        fp_id = cursor.fetchone()[0]
+        cursor.execute(fppub_sql, (fp_id, pub_id))
         cursor.execute(featprop_sql, (sf_id, cvterm_id['linked_to'], 1, f"linked_to_{i+1}_2"))
+        fp_id = cursor.fetchone()[0]
+        cursor.execute(fppub_sql, (fp_id, pub_id))
 
         # featue relationship prop pubs
         cursor.execute(feat_rel_sql, (sf_id, feature_id['symbol-{}'.format(i+1)], cvterm_id['associated_with']))
